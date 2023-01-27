8523f085294273791944b3db54b724f402bc5488
ibhati0991 <53371983+ibhati0991@users.noreply.github.com>
2023-01-27 22:04:24 +0530

    1 file modified, 1 added

--- src/app/tb-mat-table/tb-mat-table.component.ts
+++ src/app/tb-mat-table/tb-mat-table.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tb-mat-table',
   templateUrl: './tb-mat-table.component.html',
-  styleUrls: ['./tb-mat-table.component.css']
+  styleUrls: ['./tb-mat-table.component.css'],
 })
-export class TbMatTableComponent implements OnInit {
+export class TbMatTableComponent implements OnChanges {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @Input() gridModel: any;
+  @Input() dataSource: MatTableDataSource<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.dataSource.currentValue) {
+      this.dataSource.paginator = this.paginator;
+    }
   }
 
+  get rowParameter() {
+    return this.gridModel?.map((i) => i.rowParameter);
+  }
 }
