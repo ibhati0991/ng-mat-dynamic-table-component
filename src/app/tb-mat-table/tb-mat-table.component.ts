2b1438a4084e144209af88613b374b401563a6cf
ibhati0991 <53371983+ibhati0991@users.noreply.github.com>
2023-01-29 22:58:00 +0530

    4 files modified

--- src/app/tb-mat-table/tb-mat-table.component.ts
+++ src/app/tb-mat-table/tb-mat-table.component.ts
@@ -1,12 +1,6 @@
-import {
-  AfterViewInit,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
@@ -14,27 +8,28 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: './tb-mat-table.component.html',
   styleUrls: ['./tb-mat-table.component.css'],
 })
-export class TbMatTableComponent implements OnChanges, AfterViewInit {
+export class TbMatTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
-
+  @ViewChild(MatSort) sort: MatSort;
   @Input() gridModel: any;
-  @Input() dataSource: any;
+  @Input() dataSource = new MatTableDataSource<any>([]);
+  @Input() dataService :any;
   @Input() totalCount: number;
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.dataSource.currentValue) {
-      this.dataSource.paginator = this.paginator;
-    }
-    if (changes.totalCount) {
-      this.paginator.length = this.totalCount;
-    }
-  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.paginator.length = this.totalCount;
   }
 
   get rowParameter() {
     return this.gridModel?.map((i) => i.rowParameter);
+  }
+
+  getElementFromPath(element, rowParameter) {
+    rowParameter.split('.').forEach((r) => {
+      element = element[r];
+    });
+    return element;
   }
 }
