0ddc409daeedad638eb3dca0e39f13db3e21161c
ibhati0991 <53371983+ibhati0991@users.noreply.github.com>
2023-02-11 17:18:54 +0530

    3 files modified

--- src/app/components/tb-date-slider/tb-date-slider.component.ts
+++ src/app/components/tb-date-slider/tb-date-slider.component.ts
@@ -8,52 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class TbDateSliderComponent implements OnInit {
   min: number = 0;
   max: number = 100;
-  step: number = 5;
-
+  step: number = 1;
   value: number = 10;
   highValue: number = 80;
 
+  left: any ;
+  right: any ;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateRangeBar();
+  }
 
-let rangeMin = 100;
-const range = document.querySelector(".range-selected");
-const rangeInput = document.querySelectorAll(".range-input input");
-const rangePrice = document.querySelectorAll(".range-price input");
-
-rangeInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let minRange = parseInt(rangeInput[0].value);
-    let maxRange = parseInt(rangeInput[1].value);
-    if (maxRange - minRange < rangeMin) {
-      if (e.target.className === "min") {
-        rangeInput[0].value = maxRange - rangeMin;
-      } else {
-        rangeInput[1].value = minRange + rangeMin;
-      }
+  upateRange(event, type) {
+    this.updateRangeBar();
+    if (type === 'value') {
     } else {
-      rangePrice[0].value = minRange;
-      rangePrice[1].value = maxRange;
-      range.style.left = (minRange / rangeInput[0].max) * 100 + "%";
-      range.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
     }
-  });
-});
+  }
 
-rangePrice.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let minPrice = rangePrice[0].value;
-    let maxPrice = rangePrice[1].value;
-    if (maxPrice - minPrice >= rangeMin && maxPrice <= rangeInput[1].max) {
-      if (e.target.className === "min") {
-        rangeInput[0].value = minPrice;
-        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-      } else {
-        rangeInput[1].value = maxPrice;
-        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-      }
-    }
-  });
-});
+  updateRangeBar(){
+    this.left = `${(this.value/this.max) * 100}%`;
+    this.right = `${100 - (this.highValue/this.max) * 100}%`;
+  }
 }
